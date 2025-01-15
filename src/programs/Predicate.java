7e0f3a0cad03e410f636f0ddc86afcca23eb1344
xYviil <viniciusmeurer2012@gmail.com>
2025-01-14 22:50:33 -0300

    2

--- src/programs/Predicate.java
+++ src/programs/Predicate.java
@@ -1,2 +1,32 @@
-package programs;public class Predicate {
+package programs;
+
+import entities.Product;
+import util.ProductPredicate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Predicate {
+
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD case", 80.90));
+
+        //double min = 100;
+        //Predicate<Product> pred = p -> p.getPrice() >= 100.0;  <- PODE SUBSTITUIR POR "MIN".
+        //list.removeIf(pred);
+
+        list.removeIf(new ProductPredicate());
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+
+    }
+
 }
--- src/util/ProductPredicate.java
+++ src/util/ProductPredicate.java
@@ -1,2 +1,13 @@
-package util;public class ProductPredicate {
+package util;
+
+import entities.Product;
+
+import java.util.function.Predicate;
+
+public class ProductPredicate  implements Predicate<Product> {
+
+    @Override
+    public boolean test(Product product) {
+        return product.getPrice() >= 100;
+    }
 }
